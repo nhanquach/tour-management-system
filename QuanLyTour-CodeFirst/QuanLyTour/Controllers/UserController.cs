2776af13a235d01337f3daaa00e3b3ed5968e7e5
Nhân Quách <xomix1802@gmail.com>
2018-03-27 23:43:27 +0700

    Redesign Homepage and Login Page

---
+++ QuanLyTour-CodeFirst/QuanLyTour/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using QuanLyTour.DAL;
+using QuanLyTour.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyTour.Controllers
+{
+    public class UserController : Controller
+    {
+        private TourContext db = new TourContext();
+        public ActionResult SignIn(String email, String password)
+        {
+            IQueryable<User> result = null;
+            if(!String.IsNullOrEmpty(email) || String.IsNullOrEmpty(password))
+            {
+                result = from user in db.Users
+                         where user.Email.Equals(email)
+                         where user.Password.Equals(password)
+                         select user;
+            }
+
+            if(result.ToList().Count > 0)
+            {
+                //Sign In successfully
+            }
+            return View();
+        }
+    }
+}
