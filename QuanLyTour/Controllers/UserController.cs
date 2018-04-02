1866aa0696ef6f87c7cf44adb5c986fb6c6d254a
Nhan Quach <nhanquach@kms.com.vn>
2018-04-02 13:37:03 +0700

    Add Sign Up feature

--- QuanLyTour/Controllers/UserController.cs
+++ QuanLyTour/Controllers/UserController.cs
@@ -15,23 +15,47 @@ namespace QuanLyTour.Controllers
         {
             ViewBag.error = "";
             IQueryable<User> result = null;
-            if(!String.IsNullOrEmpty(email) || !String.IsNullOrEmpty(password))
+            if(!String.IsNullOrEmpty(email) && !String.IsNullOrEmpty(password))
             {
-                result = from user in db.Users
-                         where user.Email.Equals(email)
-                         where user.Password.Equals(password)
-                         select user;
+                result = from u in db.Users
+                         where u.Email.Equals(email)
+                         where u.Password.Equals(password)
+                         select u;
 
                 if(result.FirstOrDefault() != null)
                 {
                     //Sign In successfully
-                    return View(result);
+                    return View("UserHome", result.ToList());
                 } else
                 {
                     ViewBag.error = "Email or Password not match.";
                 }
             }
-
+            return View();
+        }
+        
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(FormCollection formCollection, User user)
+        {
+            string name = formCollection["Name"];
+            string email = formCollection["Email"];
+            string password = formCollection["Password"];
+            string rePassword = formCollection["Repeat_password"];
+            bool checkPassword = (password == rePassword);
+            if (checkPassword)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return View();
+            } else
+            {
+                ViewBag.error = "Password and Repeat Password do not match.";
+            }
             return View();
         }
     }
--- QuanLyTour/Models/User.cs
+++ QuanLyTour/Models/User.cs
@@ -8,7 +8,7 @@ namespace QuanLyTour.Models
     public class User
     {
         public int ID { get; set; }
-        public int Name { get; set; }
+        public String Name { get; set; }
         public String Email { get; set; }
         public String Password { get; set; }
     }
